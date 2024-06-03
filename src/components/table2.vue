4d3130bd94132ba98bd0376202141246124852f6
UZB02 <muhsinbekmirzamatov3@gmail.com>
2024-06-03 20:18:13 +0500

    table2 edit

--- src/components/table2.vue
+++ src/components/table2.vue
@@ -2,7 +2,7 @@
     <header class="pl-14 pt-14">
         <button @click="addModal = !addModal" class="btn btn-primary">Add Student</button>
     </header>
-    <div :class="addModal ? 'p-14 flex items-center absolute justify-center w-full' : 'hidden'">
+    <div :class="addModal ? 'p-14 flex items-center absolute justify-center w-full z-50' : 'hidden'">
         <form @submit.prevent="addStudent()" class="bg-slate-300 flex flex-col gap-1 p-3 rounded-md">
             <span class="flex flex-col gap-1">
                 <label for="name">Name</label>
@@ -18,7 +18,32 @@
             </span>
         </form>
     </div>
-    <div>
+    <div class="p-5">
+        <!-- component -->
+<table class="min-w-full border-collapse block md:table">
+		<thead class="block md:table-header-group">
+			<tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+				<th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">ID</th>
+				<th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Name</th>
+				<th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Second Name</th>
+				<th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Actions</th>
+			</tr>
+		</thead>
+		<tbody v-for="(item,index) in data" :key="index" class="block md:table-row-group">
+			<tr class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
+				<td class="p-2 w-[20px] md:border md:border-grey-500 text-left block md:table-cell">{{ index +1  }}</td>
+				<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{ item.name }}</td>
+				<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">{{ item.secondName }}</td>
+				<td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+					<span class="inline-block w-1/3 md:hidden font-bold">Actions</span>
+					<button @click="EditModal(item.id)" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
+					<button @click="Delete(item.id)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button>
+				</td>
+			</tr>		
+		</tbody>
+	</table>
+    </div>
+    <!-- <div>
         <table class="p-14 flex flex-col gap-3 items-center justify-center">
             <thead class="w-full shadow-lg rounded-md">
                 <tr class=" p-1 rounded-sm w-full flex justify-between">
@@ -42,7 +67,7 @@
                 </tr>
             </tbody>
         </table>
-    </div>
+    </div> -->
        <div :class="editModal ? 'p-14 flex items-center absolute justify-center w-full' : 'hidden'">
         <form @submit.prevent="addStudent()" class="bg-slate-300 flex flex-col gap-1 p-3 rounded-md">
             <span class="flex flex-col gap-1">
